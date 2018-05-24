47bc10a9d338a84ef9d32dad4d95b4b3c9bee651
James McKinney <james@slashpoundbang.com>
2018-05-24 17:39:07 -0400

    Remove optional arguments to slim down non-distributed packages

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@ from setuptools import setup
 setup(
     name='sphinxcontrib-opencontracting',
     version='0.0.0',
-    author='Ben Webb',
-    author_email='bjwebb67@googlemail.com',
     packages=['sphinxcontrib'],
-    url='https://github.com/open-contracting/sphinxcontrib-opencontracting',
     install_requires=[
         'docutils',
         'requests',
