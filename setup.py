1afdb2cfa5a57a43666241d0558b0081feccb3d4
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-01-21 16:34:55 -0500

    Add commonmark to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.0',
     packages=['sphinxcontrib'],
     install_requires=[
+        'commonmark',
         'docutils',
         'requests',
         'ocdsextensionregistry>=0.0.8',
