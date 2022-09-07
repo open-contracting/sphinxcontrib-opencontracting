0aed77ae702ddb9f81de75027a42ff2d5e0f62c8
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-09-07 12:52:06 -0400

    chore: Fix parenthesis

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,4 +2,4 @@ import os.path
 
 
 def path(*args):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__), 'fixtures', *args))
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fixtures', *args)
